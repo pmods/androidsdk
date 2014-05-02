82d6ac6fe067cacf64e1fba59e612439753c618c
Andrew Marks <amracks@gmail.com>
2014-05-01 21:56:48 -0700

    Initial commit

---
+++ manifests/init.pp
@@ -0,0 +1,36 @@
+class androidsdk {
+
+    # User to install android SDK under
+    $devusr   = "amarks"
+
+    # Group to own android SDK dir
+    $devgrp   = "amarks"
+
+    #Default Path
+    $defpath   = "/bin:/usr/bin:/sbin:/usr/sbin"
+
+    #Android SDK Location
+    $asdk_loc  = "http://dl.google.com/android/adt/22.6.2"
+
+    #Android SDK file name
+    $asdk_version = "adt-bundle-linux-x86_64-20140321"
+
+    # Make distfiles directory
+    file { 'distdir':
+        path => "/home/$devusr/distfiles",
+        ensure => directory,
+        mode => 0755,
+        owner => $devusr,
+        group => $devusr
+    }
+
+    # Download android SDK
+    exec { 'fetch-androidsdk':
+        command => "wget $asdk_loc/$asdk_version.zip",
+        user    => $devusr,
+        cwd     => "/home/$devusr/distfiles",
+        path    => $defpath,
+        unless  => "ls /home/$devusr/distfiles/$asdk_version.zip",
+        require => File['distdir']
+    }
+}
