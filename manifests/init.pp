0379ca7c0a66cb9a3ca90e77d9ccf71830112a89
Andrew Marks <andrew.marks@navy.mil>
2014-07-09 16:51:17 -0700

    Only download the SDK

--- manifests/init.pp
+++ manifests/init.pp
@@ -9,10 +9,10 @@ class androidsdk  {
     $defpath   = "/home/$devusr/AndroidSDK/tools:/home/$devusr/AndroidSDK/platform-tools:/bin:/usr/bin:/sbin:/usr/sbin:/usr/local/bin"
 
     #Android SDK Location
-    $asdk_loc  = "http://dl.google.com/android/adt/22.6.2"
+    $asdk_loc  = "http://dl.google.com/android"
 
     #Android SDK file name
-    $asdk_version = "adt-bundle-linux-x86_64-20140321"
+    $asdk_version = "android-sdk_r23.0.2-linux"
 
     case $::operatingsystem {
         freebsd: {
@@ -39,31 +39,22 @@ class androidsdk  {
 
     # Download android SDK
     exec { 'fetch-androidsdk':
-        command => "curl -o $asdk_version.zip $asdk_loc/$asdk_version.zip",
+        command => "curl -o $asdk_version.tgz $asdk_loc/$asdk_version.tgz",
         user    => $devusr,
         cwd     => "/home/$devusr/distfiles",
         path    => $defpath,
-        unless  => "ls /home/$devusr/distfiles/$asdk_version.zip",
+        unless  => "ls /home/$devusr/distfiles/$asdk_version.tgz",
+        timeout => 0,
         require => File['distdir']
     }
 
     # Extract android SDK
     exec { 'asdk-extract':
-        command => "unzip /home/$devusr/distfiles/$asdk_version.zip",
+        command => "tar xzf /home/$devusr/distfiles/$asdk_version.tgz",
         user    => $devusr,
         cwd     => "/home/$devusr",
         path    => $defpath,
-        unless  => "ls -d /home/$devusr/$asdk_version",
+        #unless  => "ls -d /home/$devusr/$asdk_version",
         require => Exec['fetch-androidsdk']
-    }
-
-    # Move SDK to ~/AndroidSDK
-    exec { 'asdk-move':
-        command => "rm -rf AndroidSDK ; mv $asdk_version/sdk ./AndroidSDK",
-        user    => $devusr,
-        cwd     => "/home/$devusr",
-        path    => $defpath,
-        unless  => "ls -d /home/$devusr/AndroidSDK",
-        require => Exec['asdk-extract']
     }
 }
